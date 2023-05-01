ce50c62af26dc973034a1091ee2bffcd89c71d40
Kiotseaeton Lancaster <kiotseaeton@gmail.com>
2023-05-01 11:05:25 -0500

    Initial Commit

---
+++ Deserts/DesertsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesertsApp.swift
+//  Deserts
+//
+//  Created by zhongyuan liu on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct DesertsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
